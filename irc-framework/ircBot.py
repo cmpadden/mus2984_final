6f5e6a24bf640ca4ff75d7eec2f81d6bdd62c64b
Cole Padden <Cole3G@gmail.com>
2014-11-10 15:28:20 -0500

    added ability to send requests !musobt

--- irc-framework/ircBot.py
+++ irc-framework/ircBot.py
@@ -1,8 +1,9 @@
 import socket
+from time import sleep
 
-server = "irc.oftc.net"    # server
-channel = "#vtcsec"         # channel
-botnick = "ayybot"         # nick
+"""
+TESTED with irc.oftc.net
+"""
 
 
 def ping():
@@ -23,48 +24,65 @@ def sendmsg(chan, msg):
     except myError as ee:
         print "ircsock.send error: " + ee
 
-def joinchan(chan):
-    '''
-    joins a channel
-    '''
+def connectToIRC(server, port, channel, botnick):
+    """
+    Goes through the handshake of connecting to an IRC server and channel
+    """
     try:
-        ircsock.send("JOIN " + chan + "\n")
-    except myError as eee:
-        print "ircsock.send error: " + eee
+        ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ircsock.connect((server, port)) 
+        ircsock.send("USER " + botnick + " " + botnick + " " + botnick + " :MUS2984 Bot!\n")
+        ircsock.send("NICK " + botnick + "\n")
+        ircsock.send("JOIN " + channel + "\n")
 
-def respond_ayy():
-    '''
-    responds to a user that inputs "Hello mybot"
-    '''
-    try:
-        ircsock.send("PRIVMSG " + channel + " :ayy lmao was detected.\n")
-    except myError as eeee:
-        print "ircsock.send error: " + eeee
+        # print connection stats (-: left aligned, s: string)
+        print 
+        print "%-50s" %("Connected to IRC")
+        print "%-25s%-25s" % ("Server", str(server)+":"+str(port))
+        print "%-25s%-25s" % ("Channel", str(channel))
+        print "%-25s%-25s" % ("Nick", str(botnick))
+        print
 
+        # return the socket
+        return ircsock
 
-# connect and join the configured channel
-try:
-    print " ** connecting to the server ** "
-    ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ircsock.connect((server, 6667)) # connect to the server using port 6667
-    ircsock.send("USER " + botnick + " " + botnick + " " + botnick + " :This is a fun bot!\n")
-    ircsock.send("NICK " + botnick + "\n") # assign the nick to the bot
-    ircsock.send("JOIN " + channel + "\n")
+    except myError as connectError:
+        print "Error: " + connectError
 
-except myError as connectError:
-    print "Error: " + connectError
+def monitorIRC(ircsock):
+    """
+    Receive data from the server
+    """
+    while 1:
+        ircmsg = ircsock.recv(256)   # receive data from the server
+        ircmsg = ircmsg.strip('\n\r') # remove unneeded linebreaks
+ 
+        # if the server pings, respond
+        if ircmsg.find("PING :") != -1:
+            ping()
+        
+        # monitor for request using !musbot
+        #TODO add threading
 
+        if ircmsg.find(":!musbot") != -1:
+            requestNick = ircmsg[1:ircmsg.find('!')] 
+            requestMessage = ircmsg[ircmsg.find(":!musbot"):][8:].strip()
+           
+            # print the nick and request to console
+            print "%-25s%-25s%-25s" % ("REQUEST", str(requestNick), str(requestMessage))
 
-# receive data from the server
-while 1:
-    ircmsg = ircsock.recv(2048)   # receive data from the server
-    ircmsg = ircmsg.strip('\n\r') # remove unneeded linebreaks
-    print ircmsg
-   
-    if ircmsg.find("PING :") != -1: # if the server pings, respond
-        ping()
+# -------------------------------------------------------------- end modules
 
-    if ircmsg.find(":ayy lmao") != -1:
-        respond_ayy()
+server = "irc.oftc.net"    # server
+port = 6667                # port
+channel = "#mus2984"       # channel
+botnick = "musBot"         # nick
 
-    
+# connect to the irc
+ircsock = connectToIRC(server, port, channel, botnick)
+
+# sleep before monitoring
+sleep(5)
+
+# monitor the IRC connection
+monitorIRC(ircsock)
