7ce50d979eb54b21303d3b554702f1fd4bf7b8c3
Cole Padden <Cole3G@gmail.com>
2014-11-12 18:41:14 -0500

    changed irc bot to use irc library

--- irc-framework/ircBot.py
+++ irc-framework/ircBot.py
@@ -1,88 +1,119 @@
-import socket
-from time import sleep
+#! /usr/bin/env python
 
+"""A simple example bot.
+
+This is an example bot that uses the SingleServerIRCBot class from
+irc.bot.  The bot enters a channel and listens for commands in
+private messages and channel traffic.  Commands in channel messages
+are given by prefixing the text by the bot name followed by a colon.
+It also responds to DCC CHAT invitations and echos data sent in such
+sessions.
+
+The known commands are:
+
+    stats -- Prints some channel information.
+
+    disconnect -- Disconnect the bot.  The bot will try to reconnect
+                  after 60 seconds.
+
+    die -- Let the bot cease to exist.
+
+    dcc -- Let the bot invite you to a DCC CHAT connection.
 """
-TESTED with irc.oftc.net
-"""
+
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+
+class musBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, port=6667):
+        print "init"
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+        self.channel = channel
+
+    def on_nicknameinuse(self, c, e):
+        print "nick in use"
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        print "welcome"
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        print "privmsg"
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        print "pubmsg"
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        print "dccmsg"
+        c.privmsg("You said: " + e.arguments[0])
+
+    def on_dccchat(self, c, e):
+        print "dccchat"
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, cmd):
+        print "do command"
+        nick = e.source.nick
+        c = self.connection
 
 
-def ping():
+        # here a variety of commands are added to the bot with various actions
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = chobj.users()
+                users.sort()
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = chobj.opers()
+                opers.sort()
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = chobj.voiced()
+                voiced.sort()
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        elif cmd == "dcc":
+            dcc = self.dcc_listen()
+            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+                ip_quad_to_numstr(dcc.localaddress),
+                dcc.localport))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
+
+def main():
+   
+    try: 
+        channel = "#mus2984"
+        nickname = "musbot"
+        server = "irc.oftc.net"
+        port = 6667
+
+        bot = musBot(channel, nickname, server, port)
+        bot.start()
+    except mainError as detail:
+        print "Error in main: " + detail
+
+if __name__ == "__main__":
     '''
-    responds to server pings
+    ensures the main function is not executed if module is used in another script
     '''
-    try:
-        ircsock.send("PONG :Pong\n")
-    except myError as e:
-        print "ircsock.send error: " + e
-
-def sendmsg(chan, msg):
-    '''
-    sends a message to a specified channel
-    '''
-    try:
-        ircsock.send("PRIVMSG " + chan + " :" + msg + "\n")
-    except myError as ee:
-        print "ircsock.send error: " + ee
-
-def connectToIRC(server, port, channel, botnick):
-    """
-    Goes through the handshake of connecting to an IRC server and channel
-    """
-    try:
-        ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ircsock.connect((server, port)) 
-        ircsock.send("USER " + botnick + " " + botnick + " " + botnick + " :MUS2984 Bot!\n")
-        ircsock.send("NICK " + botnick + "\n")
-        ircsock.send("JOIN " + channel + "\n")
-
-        # print connection stats (-: left aligned, s: string)
-        print 
-        print "%-50s" %("Connected to IRC")
-        print "%-25s%-25s" % ("Server", str(server)+":"+str(port))
-        print "%-25s%-25s" % ("Channel", str(channel))
-        print "%-25s%-25s" % ("Nick", str(botnick))
-        print
-
-        # return the socket
-        return ircsock
-
-    except myError as connectError:
-        print "Error: " + connectError
-
-def monitorIRC(ircsock):
-    """
-    Receive data from the server
-    """
-    while 1:
-        ircmsg = ircsock.recv(256)   # receive data from the server
-        ircmsg = ircmsg.strip('\n\r') # remove unneeded linebreaks
- 
-        # if the server pings, respond
-        if ircmsg.find("PING :") != -1:
-            ping()
-        
-        # monitor for request using !musbot
-        #TODO add threading
-
-        if ircmsg.find(":!musbot") != -1:
-            requestNick = ircmsg[1:ircmsg.find('!')] 
-            requestMessage = ircmsg[ircmsg.find(":!musbot"):][8:].strip()
-           
-            # print the nick and request to console
-            print "%-25s%-25s%-25s" % ("REQUEST", str(requestNick), str(requestMessage))
-
-# -------------------------------------------------------------- end modules
-
-server = "irc.oftc.net"    # server
-port = 6667                # port
-channel = "#mus2984"       # channel
-botnick = "musBot"         # nick
-
-# connect to the irc
-ircsock = connectToIRC(server, port, channel, botnick)
-
-# sleep before monitoring
-sleep(5)
-
-# monitor the IRC connection
-monitorIRC(ircsock)
+    main()
