28c4dcc8647438713a110dd321f662f1deba3711
Cole Padden <Cole3G@gmail.com>
2014-11-12 19:45:28 -0500

    working with basic commands

--- irc-framework/ircBot.py
+++ irc-framework/ircBot.py
@@ -1,101 +1,78 @@
 #! /usr/bin/env python
 
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
 """
-
+    author:
+    filename:
+    description:
+    dependencies: irc
+                                                                                             
+"""
+global asciiArt
+asciiArt = ["                      ____        _     _ ",
+            "                     |  _ \      | |   | |",
+            "  _ __ ___  _   _ ___| |_) | ___ | |_  | |",
+            " | '_ ` _ \| | | / __|  _ < / _ \| __| | |",
+            " | | | | | | |_| \__ \ |_) | (_) | |_  |_|",
+            " |_| |_| |_|\__,_|___/____/ \___/ \__| (_)",
+            " "]
+   
 import irc.bot
 import irc.strings
 from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 
 class musBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
-        print "init"
+        """
+        initialize with the channel, nick, server, and port variables
+        """
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
 
     def on_nicknameinuse(self, c, e):
-        print "nick in use"
+        """
+        If the nick is in use, register the nick with "_" appended
+        """
+        print "Status: Nick was in use."
         c.nick(c.get_nickname() + "_")
 
     def on_welcome(self, c, e):
-        print "welcome"
+        """
+        Connect to the channel specified
+        """
+        print "Status: Connecting to " + str(self.channel) + "."
         c.join(self.channel)
 
     def on_privmsg(self, c, e):
-        print "privmsg"
+        """
+        If a private message is received, perform the command specified
+        """
+        print "Status: Received private message from " + str(e.source.nick) + " with command '" + str(e.arguments[0]) + "'"
         self.do_command(e, e.arguments[0])
 
     def on_pubmsg(self, c, e):
-        print "pubmsg"
+        """
+        If a public message is received, perform command specified
+        """
         a = e.arguments[0].split(":", 1)
         if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
             self.do_command(e, a[1].strip())
+        print "Status: Received public message from " + str(e.source.nick) + " with command '" + str(e.arguments[0]) + "'"
+
         return
 
-    def on_dccmsg(self, c, e):
-        print "dccmsg"
-        c.privmsg("You said: " + e.arguments[0])
-
-    def on_dccchat(self, c, e):
-        print "dccchat"
-        if len(e.arguments) != 2:
-            return
-        args = e.arguments[1].split()
-        if len(args) == 4:
-            try:
-                address = ip_numstr_to_quad(args[2])
-                port = int(args[3])
-            except ValueError:
-                return
-            self.dcc_connect(address, port)
 
     def do_command(self, e, cmd):
         print "do command"
         nick = e.source.nick
         c = self.connection
 
-
         # here a variety of commands are added to the bot with various actions
         if cmd == "disconnect":
             self.disconnect()
+
         elif cmd == "die":
             self.die()
-        elif cmd == "stats":
-            for chname, chobj in self.channels.items():
-                c.notice(nick, "--- Channel statistics ---")
-                c.notice(nick, "Channel: " + chname)
-                users = chobj.users()
-                users.sort()
-                c.notice(nick, "Users: " + ", ".join(users))
-                opers = chobj.opers()
-                opers.sort()
-                c.notice(nick, "Opers: " + ", ".join(opers))
-                voiced = chobj.voiced()
-                voiced.sort()
-                c.notice(nick, "Voiced: " + ", ".join(voiced))
-        elif cmd == "dcc":
-            dcc = self.dcc_listen()
-            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
-                ip_quad_to_numstr(dcc.localaddress),
-                dcc.localport))
+
         else:
             c.notice(nick, "Not understood: " + cmd)
 
@@ -109,8 +86,8 @@ def main():
 
         bot = musBot(channel, nickname, server, port)
         bot.start()
-    except mainError as detail:
-        print "Error in main: " + detail
+    except Exception, detail:
+        print "Error in main: " + str(detail)
 
 if __name__ == "__main__":
     '''
