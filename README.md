c2d48d8158baa505577975825d5c7f949541331a
cmpadden <cmpadden@vt.edu>
2014-11-24 12:28:54 -0500

    overall update

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 MUSIC 2984 Final Project
 ========================
 
-IRC bot writtin in Python that controls a synthesizer written in SuperCollider via Open Sound Control (OSC).
+...
 
-
-Dependencies
+Markups
 -------
 
 ...
--- sound-generation/interfaceOSC.py
+++ sound-generation/interfaceOSC.py
@@ -2,12 +2,22 @@
 # additionally : http://www.caseyanderson.com/teaching/ipython-to-supercollider-via-osc/
 # dependent on module : pyOSC
 
-
 import OSC
 import time, random
-client = OSC.OSCClient()
-client.connect( ( '127.0.0.1', 57120 ) )
-msg = OSC.OSCMessage()
-msg.setAddress("/print")
-msg.append(400)
-client.send(msg)
+
+
+def sendMessageOSC( frequency, msgAddress="/acid" ):
+    try:
+        client = OSC.OSCClient()
+        #client.connect( ( '127.0.0.1', 57120 ) )
+        client.connect( ( '192.168.1.21', 57120 ) )
+        msg = OSC.OSCMessage()
+        msg.setAddress(msgAddress)
+        msg.append(frequency)
+        client.send(msg)
+    except Exception, e:
+        print "Error in OSC Connection: " + str(e)
+
+
+# test the function
+sendMessageOSC(100)
